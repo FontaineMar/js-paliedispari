3df95c47552007ece215bb4f1b89019ec37a0316
marco <odri.fo@gmail.com>
2020-10-27 19:12:40 +0100

    finito

--- ex2/script.js
+++ ex2/script.js
@@ -1,17 +1,29 @@
 var generateEl = document.getElementById("generate");
 
 generateEl.addEventListener('click' , function(){
-  var selectPaDiEl = document.getElementById("selectPaDi").value;
-  var pariDisp = selectPaDiEl.value
-  var selectNumEl = document.getElementById("selectNum").value;
+  var selectPaDiEl = document.getElementById("selectPaDi");
+  var pariDisp = selectPaDiEl.value;
+  var selectNumEl = document.getElementById("selectNum");
   var numPers = selectNumEl.value
-  var numCons = Math.floor(Math.random() * (5 - 1) + 1);
+  var numCons = Math.floor(Math.random() * (6 - 1)+ 1);
+  console.log(numCons)
   var result = numPers + numCons ;
+  var message = "hai perso"
 
-    if(pariDisp == "pari")  
-    function isEven(){}
-    message
+  function evenOrNot(num){
+    if (num % 2 == 0){
+      return  'pari';
+    }
+    else {
+      return 'dispari';
+    }
+  }
+ console.log(evenOrNot(result));
+ console.log(pariDisp);
+  if(pariDisp == evenOrNot(result)){
+    message = "hai vinto";
+  }
 
-
+  document.getElementById('morra').innerHTML = message
 
 })
